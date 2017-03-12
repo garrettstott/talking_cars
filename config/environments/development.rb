5a6a568e91f4e6b2b6a035f495da05ffb01673f3
garrettstott <garrettstott@gmail.com>
2017-03-12 16:18:13 -0600

    fix button_tag

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   # SMTP settings for gmail
 
   config.action_mailer.smtp_settings = {
